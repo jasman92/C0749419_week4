d0bd4d87e325cbe9f349c907cc271a867f2a7554
visualstudio <visualstudio@230-PC34>
2019-01-31 11:47:55 -0500

    i created online event

--- week4/js/main.js
+++ week4/js/main.js
@@ -1,1 +1,9 @@
-﻿// Your code here!
+﻿
+//how to put this code into a function called myFunction
+
+function myFunction() {
+
+
+    var cat = new Date();
+    document.body.innerHTML = "<h1> The date is " + cat.getMonth() + " / " + cat.getDate() + " / " + cat.getFullYear() + " </h1>"
+}
